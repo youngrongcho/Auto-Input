7f8b9591f742b7b0e087217d94a726b745f2f11e
youngrong <whdudfhd1127@naver.com>
2023-07-16 19:55:46 +0900

    [Fix] method parameter 추가

--- input/src/main/java/auto/input/service/Service.java
+++ input/src/main/java/auto/input/service/Service.java
@@ -1,17 +1,9 @@
 package auto.input.service;
 
+import auto.input.dto.Dto;
+
 import java.io.IOException;
 
 public interface Service {
-
-    public void input() throws IOException;
-
-    //entity 생성
-    //해당 연도-월의 모든 날짜에 대하여, 모든 시간을 input
-
-    //주말이면 상태 ban
-    //영업 종료시간이어도 상태 ban
-    //점심시간이면 상태 lunch
-
-    //저장하여 repository insert
+    public void input(Dto dto) throws IOException;
 }
